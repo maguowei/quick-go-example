ddf481b5f61bdb4f4b25537687a8a6d07ae67ad5
maguowei <imaguowei@gmail.com>
2021-11-27 20:30:49 +0800

    quick-go auto update & commit example

--- internal/example/app.go
+++ internal/example/app.go
@@ -2,34 +2,35 @@ package example
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/maguowei/example/internal/pkg/app"
 	"github.com/maguowei/example/internal/example/interface/routers"
 	"github.com/maguowei/example/internal/pkg/configs"
 )
 
-var app App
+var myApp app.App
 
-type App struct {
+type DefaultApp struct {
 	appName string
 	addr   string
 	router *gin.Engine
 }
 
-func (app *App) init() {
+func (app *DefaultApp) init() {
 	configs.InitConfig()
 	routers.InitRouter()
 	app.router = routers.GetRouter()
 }
 
-func (app *App) Run() {
+func (app *DefaultApp) Run() {
 	app.init()
 	app.router.Run()
 }
 
-func NewApp() *App {
-	app = App{}
-	return &app
+func NewApp() app.App {
+	myApp = &DefaultApp{}
+	return myApp
 }
 
-func GetApp() *App {
-	return &app
+func GetApp() app.App {
+	return myApp
 }
