83b73f8a4e3dd7b196c1ba123d54d174e11f6943
maguowei <imaguowei@gmail.com>
2022-01-08 20:06:25 +0800

    quick-go auto update & commit example

--- internal/example/domain/repository/example.go
+++ internal/example/domain/repository/example.go
@@ -1,13 +1,1 @@
 package repository
-
-import (
-	"github.com/maguowei/example/internal/example/domain/entities"
-)
-
-type ExampleRepository interface {
-	GetExample(exampleId int64) (entities.Example, error)
-	GetExamples() ([]entities.Example, error)
-	CreateExample(example entities.Example) (entities.Example, error)
-	UpdateExample(example entities.Example) (entities.Example, error)
-	DeleteExample(exampleId int64) error
-}
---
+++ internal/example/domain/repository/interface.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"github.com/maguowei/example/internal/example/domain/entities"
+)
+
+type ExampleRepository interface {
+	GetExample(exampleId int64) (entities.Example, error)
+	GetExamples() ([]entities.Example, error)
+	CreateExample(example entities.Example) (entities.Example, error)
+	UpdateExample(example entities.Example) (entities.Example, error)
+	DeleteExample(exampleId int64) error
+}
